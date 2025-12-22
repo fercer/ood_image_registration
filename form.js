3fbb03650060ae7ab556daf7ff59ec32ed2bf925
Fernando Cervantes Sanchez <iie.fercer@gmail.com>
2025-12-22 15:14:19 -0500

    Reading arguments programmatically in script.sh.erb

--- form.js
+++ form.js
@@ -8,23 +8,25 @@
     const bridge_id = "batch_connect_session_context_json_bridge";
 
     // Function to toggle visibility
-    function toggle_visibility(is_checked, field_id) {
-        let element = document.getElementById(field_id);
-        
-        if (element) {
-            if (is_checked) {
-                element.closest(".mb-3").style.display = '';
-            } else {
-                element.closest(".mb-3").style.display = 'none';
+    function toggle_visibility(is_checked, widget_arguments) {
+        Object.entries(widget_arguments).forEach(function(argument_entry){
+            let element = document.getElementById(argument_entry[0]);
+            
+            if (element) {
+                if (is_checked) {
+                    element.closest(".mb-3").style.display = '';
+                } else {
+                    element.closest(".mb-3").style.display = 'none';
+                }
             }
-        }
+        });
     }
 
     // Function to toggle visibility of show checkboxes
-    function toggle_show_advanced_visibility(selected_preprocessor, preprocessors_hash) {
-        Object.entries(preprocessors_hash).forEach(function(preprocessor_entry){
+    function toggle_show_advanced_visibility(selected_preprocessor, preprocessors_dict) {
+        Object.entries(preprocessors_dict).forEach(function(preprocessor_entry){
             const preprocessor_name = preprocessor_entry[0];
-            const show_preprocessor_id = preprocessor_entry[1];
+            const show_preprocessor_id = preprocessor_entry[1]["widget_id"];
 
             let show_preprocessor_checkbox = document.getElementById(show_preprocessor_id);
 
@@ -50,36 +52,34 @@
 
             // 3. Parse it into a Javascript Object
             try {
-                const advanced_arguments_data = JSON.parse(raw_json);
+                const advanced_args_ids = JSON.parse(raw_json);
                 
                 // DEBUG: Confirm it worked
-                console.log("Loaded Data from Bridge:", advanced_arguments_data);
+                console.log("Loaded Data from Bridge:", advanced_args_ids);
 
-                Object.entries(advanced_arguments_data["show_checkboxes"]).forEach(function(advanced_arguments_entry) {
-                    const show_preprocessor_id = advanced_arguments_entry[0];
-                    const preprocessor_args_list = advanced_arguments_entry[1];
+                Object.entries(advanced_args_ids).forEach(function(advanced_arguments_entry) {
+                    const preprocessor_id = advanced_arguments_entry[0];
+                    const preprocessor_opts_dict = advanced_arguments_entry[1];
 
-                    const show_preprocessor_checkbox = document.getElementById(show_preprocessor_id);
-                    
-                    preprocessor_args_list.forEach(function(argument_id) {
-                        show_preprocessor_checkbox.addEventListener('change', function() {
-                            toggle_visibility(this.checked, argument_id);
-                        });
-                        toggle_visibility(show_preprocessor_checkbox.checked, argument_id);
-                    });
-                });
-
-                Object.entries(advanced_arguments_data["dropdowns"]).forEach(function(element_entry) {
-                    const element_id = element_entry[0];
-                    const preprocessors = element_entry[1];
-
-                    const preprocessor_dropdown = document.getElementById(element_id);
-
+                    const preprocessor_dropdown = document.getElementById(preprocessor_id);
                     preprocessor_dropdown.addEventListener('change', function() {
-                        toggle_show_advanced_visibility(this.value, preprocessors)
+                        toggle_show_advanced_visibility(this.value, preprocessor_opts_dict)
                     });
+                    toggle_show_advanced_visibility(preprocessor_dropdown.value, preprocessor_opts_dict)
 
-                    toggle_show_advanced_visibility(preprocessor_dropdown.value, preprocessors)
+                    Object.entries(preprocessor_opts_dict).forEach(function(opts) {
+                        const show_preprocessor_id = opts[1]["widget_id"];
+                        const widget_arguments = opts[1]["widget_arguments"];
+
+                        const show_preprocessor_checkbox = document.getElementById(show_preprocessor_id);
+
+                        if (show_preprocessor_checkbox) {
+                            show_preprocessor_checkbox.addEventListener('change', function() {
+                                toggle_visibility(this.checked, widget_arguments);
+                            });
+                            toggle_visibility(show_preprocessor_checkbox.checked, widget_arguments);
+                        }
+                    });
                 });
             } catch (e) {
                 console.error("Failed to parse JSON from bridge:", e);
